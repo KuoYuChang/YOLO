a261976f2d48df8f117431cf739506628c57993b
KuoYuChang <kuoyuchang25@gmail.com>
2026-02-09 20:55:19 +0800

    [Add] show loss in training epochs

--- yolo/tools/solver_pure.py
+++ yolo/tools/solver_pure.py
@@ -136,6 +136,7 @@ class TrainSolver(ValidateSolver):
 
         print(f"------------ start epoch {epoch_ith} ------------")
 
+        running_loss = 0.0
 
         self.model.train()
         
@@ -153,6 +154,8 @@ class TrainSolver(ValidateSolver):
             # loss
             loss, loss_item = self.loss_fn(aux_predicts, main_predicts, targets)
             loss.backward()
+
+            running_loss +=loss.item()
             
             # gradient optimize
             self.optimizer.step()
@@ -160,7 +163,8 @@ class TrainSolver(ValidateSolver):
             
             if batch_idx % self.train_print_num == 0:
                 current_time = time.time() - start_t
-                print(f'time elapsed: {current_time}, finish {batch_idx/len(self.train_loader)}')
+                print(f'time elapsed: {current_time}, loss: {running_loss / print_num:.3f}, finish {batch_idx/len(self.train_loader)}')
+                running_loss = 0.0
 
         # scheduler, current fix as epoch-wise
         self.scheduler.step()
